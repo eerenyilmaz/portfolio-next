6eb93dbc4640fda1721adf08be68373dce87cdc6
Eren Yılmaz <eeyilmaz34@gmail.com>
2023-03-26 04:35:20 +0300

    HomePage views added as a component in to the index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,8 @@
 import { useState } from 'react';
 import Link from 'next/link';
+import HomePage from '../src/views/HomePage';
 
-function Header({ title }) {
-  return <h1>{title ? title : 'Default title'}</h1>;
-}
+export default function Home() {
 
-export default function HomePage() {
-  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
-
-  const [likes, setLikes] = useState(0);
-
-  function handleClick() {
-    setLikes(likes + 1);
-  }
-
-  return (
-    <div>
-      <h1 className="title">
-        Read <Link href="/posts/first-post">this page!</Link>
-      </h1>
-    </div>
-  );
+  return <HomePage title={"Header"}/>
 }
