908c98432d04e12a42e78e7d25ea286a94ab053f
Eren Yılmaz <eeyilmaz34@gmail.com>
2023-04-01 04:24:16 +0300

    Header fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,13 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="w-full bg-red-200 color-red-500 h-12">
+        <header className="w-full bg-red-700 color-red-500 h-16">
             <div className="flex items-center h-full content shadow md:shadow-none justify-center">
-                <div className="flex items-center justify-between w-full gap-x-1 mx-auto max-w-screen-md">
-                    <Link className="font-medium text-red-800" href="/posts/first-post">Erol Eren Yılmaz</Link>
-
-                    <div className="flex gap-4 md:gap-6 text-gray-500">
-                        <Link href="/about">About</Link>
-                        <Link href="/posts/first-post">Blogs</Link>
+                <div className="flex items-center w-full gap-x-1 mx-auto max-w-screen-md justify-center">
+                    <div className="flex gap-4 md:gap-6 text-gray-500 items-end">
+                        <Link className="text-xl text-yellow-500" href="/about">About</Link>
+                        <Link className="font-medium text-white text-2xl" href="/">Erol Eren Yılmaz</Link>
+                        <Link className="text-xl text-yellow-500" href="/posts/first-post">Blogs</Link>
                     </div>
                 </div>
             </div>
