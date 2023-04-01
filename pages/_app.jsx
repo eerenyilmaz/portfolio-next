b0a9a940677f4ec043a280ebed655a30dd7596de
Eren Yılmaz <eeyilmaz34@gmail.com>
2023-04-01 04:23:27 +0300

    Header fixed as a constant

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,20 @@
 import '../styles/global.css'
 import 'tailwindcss/tailwind.css'
+import Header from '../src/components/Header';
 
  function MyApp({ Component, pageProps }) {
-   return <Component {...pageProps} />
+   return (
+    <>
+      <Header />
+      <main className='flex min-h-screen py-32 overflow-hidden'>
+        <div className='w-full h-full'>
+          <div className='w-full gap-x-1 mx-auto max-w-screen-md'>
+            <Component {...pageProps} />
+          </div>
+        </div>
+      </main>
+    </>
+   )
  }
 
  export default MyApp
