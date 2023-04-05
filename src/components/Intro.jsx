266829e5240a6285696ba0703926ddb7df1bff42
eerenyilmaz <eeyilmaz34@gmail.com>
2023-04-05 16:13:45 +0300

    Component and Image added

---
+++ src/components/Intro.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Intro() {
+    return (
+        <div className="w-full h-screen bg-image">
+            <div className="intro-bg h-full">
+
+            </div>
+        </div>
+    )
+}
