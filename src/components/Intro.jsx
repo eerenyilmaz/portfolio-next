eaea88bf1cefe4780c42d8397dacc4fa0a717bed
eerenyilmaz <eeyilmaz34@gmail.com>
2023-04-10 16:53:32 +0300

    Background changed and added some changes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,8 +3,8 @@
 export default function Intro() {
     return (
         <div className="w-full h-screen bg-image">
-            <div className="intro-bg h-full">
-
+            <div className="intro-bg h-full flex items-center justify-center">
+                <span className="text-white">Hello, I'm Erol Eren Yılmaz</span>
             </div>
         </div>
     )
