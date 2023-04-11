5892c5b0755e345bffcf2d7385c65fd431ea688b
Eren Yılmaz <eeyilmaz34@gmail.com>
2023-04-12 01:36:06 +0300

    Header fixed

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -2,7 +2,7 @@
 
 export default function PageHeader({title}) {
     return (
-        <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-red-600 relative inline-block">
+        <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-red-600 relative inline-block mb-10">
             <span className="text-4xl relative text-white">{title}</span>
         </span>
     )
