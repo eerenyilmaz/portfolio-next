93e2e79e24af84e54459b720e37ce2092a96bef2
eerenyilmaz <eeyilmaz34@gmail.com>
2023-04-19 14:45:06 +0300

    Icons hovers fixed

--- src/components/SocialIcon.jsx
+++ src/components/SocialIcon.jsx
@@ -1,6 +1,6 @@
 export default function SocialIcon({path,link}){
     return(
-        <li className="mb-2 inline-block rounded flex items-center cursor-pointer w-12 p-3 hover:p-2 transition duration-500 text-xs font-medium uppercase leading-normal text-white bg-red-600 shadow-md ease-in-out mx-2 ">
+        <li className="mb-2 inline-block rounded flex items-center cursor-pointer w-12 p-3 hover:bg-gray-600 hover:text-red-600 transition duration-500 text-xs font-medium uppercase leading-normal text-white bg-red-600 shadow-md ease-in-out mx-2 ">
             <a href={link} target="_blank" className="w-full"> 
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
