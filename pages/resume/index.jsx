cca7d57208db4f99499443aca962554457938c2b
eerenyilmaz <eeyilmaz34@gmail.com>
2023-04-24 17:09:03 +0300

    resume folder added in pages folder

---
+++ pages/resume/index.jsx
@@ -0,0 +1,44 @@
+import PageHeader from "../../src/components/PageHeader"
+import ResumeDetail from "../../src/components/ResumeDetail"
+
+
+export default function Resume(){
+    return (
+        <div className="max-w-screen-md mb-20">
+            <PageHeader title={"Resume"}/>
+
+            <ResumeDetail 
+                year={2021}
+                name={"Z3 Yazılım ve Teknoloji"}
+                description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
+            />
+
+            <ResumeDetail 
+                year={2021}
+                name={"Mezuniyet"}
+                description={"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"}
+            />
+
+            <ResumeDetail 
+                year={2018}
+                name={"Kanal 7 / Staj"}
+                description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
+            />
+
+            <ResumeDetail 
+                year={2017}
+                name={"Halkbank BT / Staj"}
+                description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
+            />
+            
+            <ResumeDetail 
+                year={2016}
+                name={"T.C İstanbul Ticaret Üniversitesi"}
+                description={"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"}
+                border={false}
+            />
+        
+            
+        </div>
+    )
+}
