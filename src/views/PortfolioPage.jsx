9d3564d9c64a5490236f08232fac7c31e358d620
eerenyilmaz <eeyilmaz34@gmail.com>
2023-04-26 12:05:15 +0300

    Design has created in Potrfolio Page

---
+++ src/views/PortfolioPage.jsx
@@ -0,0 +1,10 @@
+import PortfolioCard from "../components/PortfolioCard";
+
+export default function PortfolioPage(){
+    return(
+        <div className="grid md:grid-cols-3 grid-cols-1 gap-4">
+            <PortfolioCard />
+          
+        </div>
+    )
+}
