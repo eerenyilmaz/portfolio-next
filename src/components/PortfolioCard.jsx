bac1727df73f0c8a6de2978c7a25029a0a410901
eerenyilmaz <eeyilmaz34@gmail.com>
2023-04-26 17:20:13 +0300

    Portfolio card and area completed

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,9 +1,27 @@
-export default function PortfolioCard() {
+
+export default function PortfolioCard({header, description, imageUrl, language}) {
     return(
-        <div>
-            <div></div>
-            
-            <div></div>
+        <div className="portfolio-card border-gray-200 bg-red-800 hover:bg-red-600 transition duration-300">
+            <div className="card-top" >
+                {/* <img src="../logo.svg" /> */}
+                {/* Image sorununu hallet. Dinamik link*/}
+            </div>
+
+            <div className="card-bottom">
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2 text-red-200 flex">
+                        <span className="self-start">{ header }</span>
+                    </div>
+                    <p className="text-gray-700 text-base text-red-200">
+                        {description}
+                    </p>
+                </div>
+                <div className="px-6 pt-4 pb-2">
+                    <span className="inline-block bg-yellow-800 rounded px-2 py-1 text-sm font-semibold text-yellow-300 mr-2 mb-2">
+                        { language}
+                    </span>
+                </div>
+            </div>
         </div>
     )
 }
--- src/views/PortfolioPage.jsx
+++ src/views/PortfolioPage.jsx
@@ -2,9 +2,33 @@ import PortfolioCard from "../components/PortfolioCard";
 
 export default function PortfolioPage(){
     return(
-        <div className="grid md:grid-cols-3 grid-cols-1 gap-4">
-            <PortfolioCard />
-          
+        <div className="grid md:grid-cols-3 grid-cols-1 gap-4 h-full">
+            <PortfolioCard 
+                header={"Project Header"} 
+                description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."} 
+                language={"React"}   
+                imageUrl={""} 
+            />
+            <PortfolioCard 
+                header={"Project Header"} 
+                description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."} 
+                language={"React"}   
+                imageUrl={""} 
+            />
+            <PortfolioCard 
+                header={"Project Header"} 
+                description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."} 
+                language={"React"}   
+                imageUrl={""} 
+            />
+            <PortfolioCard 
+                header={"Project Header"} 
+                description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."} 
+                language={"React"}   
+                imageUrl={""} 
+            />
+            
+        
         </div>
     )
 }
