0b7b783b21b4134818bd4c7d648fcfd198cadec6
eerenyilmaz <eeyilmaz34@gmail.com>
2023-05-02 15:45:56 +0300

    Font added in to the project

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,15 @@
 import '../styles/global.css'
 import 'tailwindcss/tailwind.css'
 import Header from '../src/components/Header';
+import { Nunito } from 'next/font/google'
+
+const nunito = Nunito({ subsets: ['latin'] })
 
  function MyApp({ Component, pageProps }) {
    return (
     <>
       <Header />
-      <main className='flex min-h-screen overflow-hidden'>
+      <main className={"{ 'flex min-h-screen overflow-hidden'}",nunito.className}>
         <div className='w-full h-full'>
           <div className='w-full gap-x-1'>
             <Component {...pageProps} />
