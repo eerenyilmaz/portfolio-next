ea9820f0b9c4eb03da6c1c6a308644313c0d42a3
erenyilmaz <eerenyilmaz34@gmail.com>
2023-05-04 21:13:14 +0300

    Footer fixed for responsive design

--- src/views/Footer.jsx
+++ src/views/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer(){
 
     return(
-        <footer className="w-full flex items-center justify-center h-auto py-20 bg-dark-blue">
-            <div className="max-w-screen-lg color-light-blue font-bold text-xl">
+        <footer className="w-full flex items-center justify-center h-auto py-10 md:py-20 bg-dark-blue">
+            <div className="max-w-screen-lg color-light-blue font-bold text-sm md:text-xl">
                 <span className="me-2">© Copyright 2023</span>
                 <span className="color-blue">EEY</span>
             </div>
